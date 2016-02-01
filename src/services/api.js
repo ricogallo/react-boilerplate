77cb6f9638e1395a91a302ee51859a6186e0c99d
mfirry <mfirry@gmail.com>
2016-02-01 15:55:10 +0100

    minor

--- src/services/api.js
+++ src/services/api.js
@@ -6,15 +6,7 @@ class Api {
   github() {
     const fetch = f(Promise).fetch;
     return fetch('https://api.github.com/users/unicredit');
-      // .then(function(response) {
-      //   return response.json()
-      // }).then(function(json) {
-      //   console.log('parsed json', json)
-      //   return json;
-      // }).catch(function(ex) {
-      //   console.log('parsing failed', ex)
-      // });
-    }
+  }
 }
 
 exports.Api = Api;
