ec28adc60788f47a02fde7fda6137b4eac3396cf
mfirry <mfirry@gmail.com>
2016-03-08 10:17:59 +0100

    react-router version bumped

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import { Router, Route } from 'react-router';
+import { Router, Route, hashHistory } from 'react-router';
 
 import Main from './components/Main';
 import PoweredBy from './components/Powered-by';
 import About from './components/About';
 
 render((
-  <Router>
+  <Router history={ hashHistory } >
     <Route path="/" component={Main}>
       <Route path="/about" component={About} />
       <Route path="/poweredby" component={PoweredBy} />
