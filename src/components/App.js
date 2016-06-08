09eb167881ce53d72b8c43a52c3bfd18261262b5
Fumarola Fabio (UniCredit) <fabio.fumarola@unicredit.eu>
2016-06-08 22:58:53 +0200

    update

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,19 @@ import React, { PropTypes } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 
+
+const Default = (
+  <div>
+    Welcome to React Boilerplate
+  </div>
+)
+
 const App = React.createClass({
   render () {
     return (
       <div>
         <Header />
-        {this.props.children || 'Welcome to React Boilerplate' }
+        {this.props.children || Default  }
         <Footer />
       </div>
     )
